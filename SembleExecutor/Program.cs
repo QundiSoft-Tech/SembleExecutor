29403df46511e14a6eca7773ee38ab05c32b666b
Rishi Varma Kalidindi <rishivarmak.developer@gmail.com>
2022-12-19 03:36:00 -0500

    Created Project Structure

--- SembleExecutor/Program.cs
+++ SembleExecutor/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SembleExecutor
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine();
+        }
+    }
+}
